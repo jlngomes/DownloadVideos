5117416502b7ca77299fe0be4133faf4a1a044a1
Jean Luca <jean.luca162021@outlook.com>
2025-04-13 23:27:40 -0300

    V1.1 - Adding a download function for the channel that my church always has to download

--- script.py
+++ script.py
@@ -1,14 +1,86 @@
-# Importa a biblioteca yt_dlp, que serve para baixar vídeos do YouTube
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from time import sleep
+from datetime import datetime
 import yt_dlp
-# Importa a biblioteca os, que ajuda a mexer com pastas e arquivos no computador
 import os
 
+PATH = f'{os.getcwd()}\\download'
 
-# Define uma função chamada downloadYouTube, que vai baixar o vídeo
-# Ela precisa de dois "ingredientes": a URL do vídeo (videourl) e o lugar onde o vídeo vai ser salvo (path)
+def data_tratatamento(data):
+    data_str = str(data)
+    data_lista = data_str.split('-')
+    dict = {
+            "01":"Jan",
+            "02":"Fev",
+            "03":"Mar",
+            "04":"Abr",
+            "05":"Mai",
+            "06":"Jun",
+            "07":"Jul",
+            "08":"Ago",
+            "09":"Sep",
+            "10":"Oct",
+            "11":"Nov",
+            "12":"Dec",
+    }
+    return data_lista[2], dict.get(data_lista[1]), data_lista[0]
+
+def baixar_primeiro_video(link: str):
+    data_atual = datetime.today().date()
+
+    dia, mes, ano = data_tratatamento(data=data_atual)
+
+    # Configura o modo headless
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Não mostra a interface gráfica
+    chrome_options.add_argument("--disable-gpu") # Melhora compatibilidade em alguns sistemas
+    chrome_options.add_argument("--mute-audio")
+    chrome_options.add_argument("--window-size=1920,1080")  # Define um tamanho de janela padrão
+    chrome_options.add_argument("--log-level=3")
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(link)
+    sleep(5)
+
+    driver.maximize_window()
+
+    sleep(1)
+    print("\nAbrindo o canal do Provai e Vede...")
+
+    for aba in driver.find_elements(By.CLASS_NAME, 'yt-tab-shape-wiz__tab'):
+        if aba.text == 'Vídeos':
+            aba.click()
+            sleep(3)
+            break
+
+    sleep(1)
+    print("\nFiltrando/Procurando o video de hoje...")
+
+    for video in driver.find_elements(By.CLASS_NAME,"style-scope.ytd-rich-grid-renderer"):
+        if 'Provai e Vede' in  video.text:
+            if f'{ano} ({dia}/{mes})' in video.text and not 'Libras' in video.text:
+                video.click()
+                sleep(3)
+
+    print("\nPegando o link...\n")
+
+    link_video = driver.current_url
+
+    driver.quit()
+
+    return link_video
+
 def downloadYouTube(videourl, path):
-    # Verifica se a pasta onde o vídeo vai ser salvo existe
-    # Se não existir, cria ela com o comando os.makedirs
+    """
+    Função que realiza o download de videos do Youtube que ultilizando a biblioteca "yt_dlp"
+
+    :parameter
+    - URL de video desejado
+    - Path de redirecionamento do video instalado
+
+    """
     if not os.path.exists(path):
         os.makedirs(path)
 
@@ -35,12 +107,44 @@ def downloadYouTube(videourl, path):
         # Mostra uma mensagem de erro com o problema que aconteceu
         print(f"Erro ao baixar o vídeo: {e}")
 
+while True:
+    choice = input('\n[1] = Informativo mundial das missões\n'
+                       '[2] = Provai e Vede\n'
+                       '[3] = Musíca\n'
+                       '[4] = Sair\n'
+                       'O que deseja fazer: ')
 
-# Pede para o usuário digitar a URL do vídeo do YouTube
-url = input("\nDigite a URL do vídeo do YouTube: ")
-# Define o caminho onde o vídeo vai ser salvo
-# Usa o lugar atual do computador (os.getcwd) e adiciona uma pasta chamada "download"
-path = f'{os.getcwd()}\\download'
+    if not choice.isdigit():
+        print("\nOpção inválida, tente novamente\n")
+        continue
 
-# Chama a função downloadYouTube com a URL e o caminho que definimos
-downloadYouTube(videourl=url, path=path)
+    choice = int(choice)
+
+    opcao = [1,2,3,4]
+    if not choice in opcao:
+        print("\nOpção inválida, tente novamente\n")
+        continue
+
+    if choice == 1:
+        # link_informativo = ""
+        # link = baixar_primeiro_video()
+        # downloadYouTube(videourl=link, path=PATH)
+        print("\nEm desenvolvimento...\n")
+        continue
+
+    if choice == 2:
+        link_provai = "https://www.youtube.com/@provaivedeoficial"
+        link = baixar_primeiro_video(link=link_provai)
+        downloadYouTube(videourl=link, path=PATH)
+        continue
+
+    if choice == 3:
+        url = input("\nDigite a URL do vídeo do YouTube: ")
+        downloadYouTube(videourl=url, path=PATH)
+        continue
+
+    else:
+        print("\nObrigado por escolher nossos serviços! ❤️️\n"
+              "\nBy: Jean Luca Novaes Gomes\n")
+        break
+
